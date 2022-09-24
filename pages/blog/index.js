82faab0e8270e737aa1e7f7b0e9ef2ca44f1af0d
Joe di Stefano <joeydi@okaypl.us>
2022-09-23 21:15:18 -0400

    blog index pagination button, etc

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
+import Link from "next/link";
 import { getPaginatedPosts } from "@/lib/blog";
 import Post from "@/components/post";
+
+import ChevronRight from "@/icons/chevron-right.svg";
 
 export default function Blog({ posts }) {
     return (
@@ -22,6 +25,22 @@ export default function Blog({ posts }) {
                             {posts.map((post, i) => (
                                 <Post post={post} key={i} />
                             ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="blog-pagination">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-md-10 col-lg-8 d-flex">
+                                <div className="next ms-auto">
+                                    <Link href="/blog/2">
+                                        <a className="btn">
+                                            Next Page
+                                            <ChevronRight className="icon ms-5" />
+                                        </a>
+                                    </Link>
+                                </div>
                         </div>
                     </div>
                 </div>
