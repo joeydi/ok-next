860b2036b03879f972fa5ed46c7ca5d39dc20349
Joe di Stefano <joeydi@okaypl.us>
2022-09-29 13:45:35 -0400

    finish twitter-bio

--- pages/twitter-bio/index.js
+++ pages/twitter-bio/index.js
@@ -1,9 +1,9 @@
+import sample from "underscore/modules/sample.js";
+import random from "underscore/modules/random.js";
 import Head from "next/head";
-import data from './data.json';
+import data from "./data.json";
 
-export default function TwitterBio() {
-    console.log(data);
-
+export default function TwitterBio({ bio }) {
     return (
         <div className="blog">
             <Head>
@@ -12,9 +12,7 @@ export default function TwitterBio() {
             <div id="hero">
                 <div className="container">
                     <div className="row">
-                        <h1 className="col-lg-10">
-                            Your New Twitter Bio
-                        </h1>
+                        <h1 className="col-lg-10">Your New Twitter Bio</h1>
                     </div>
                 </div>
             </div>
@@ -22,11 +20,30 @@ export default function TwitterBio() {
                 <div className="container">
                     <div className="row justify-content-center">
                         <div className="col-md-10 col-lg-8">
-                            Testing
+                            <h2>{bio}</h2>
+                            <hr />
+                            <p>This is an implementation of Merlin Mann&rsquo;s formula for Twitter profile descriptions, the inspiration for which is below. I thought it would be fun to bring it to life, so I scraped together a variety of sources to populate the different values, and wrote a little script to piece them together randomly.</p>
+                            <blockquote>
+                                <p>
+                                    function twitterbio {"{"} echo $realJob, $aspirationalJob, $familyTitle, and $whackadoodleHobby. $techGerund since $yearStarted. {"}"}
+                                </p>
+                                <p>
+                                    — Merlin Mann (@hotdogsladies) <br />
+                                    <a href="https://twitter.com/hotdogsladies/statuses/165906670797594624">February 4, 2012</a>
+                                </p>
+                            </blockquote>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     );
+}
+
+export async function getServerSideProps() {
+    const { realJobs, aspirationalJobs, whackadoodleHobbies, familyTitles, techGerunds } = data;
+    const year = new Date().getFullYear() - random(10);
+    const bio = `${sample(realJobs)}, ${sample(aspirationalJobs)}, ${sample(familyTitles)}, and ${sample(whackadoodleHobbies)}. ${sample(techGerunds)} since ${year}.`;
+
+    return { props: { bio } };
 }
