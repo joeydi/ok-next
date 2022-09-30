58a52676150a00b70741d8e22a3e19feffb2d5e7
Joe di Stefano <joeydi@okaypl.us>
2022-09-29 22:04:46 -0400

    add GA4 analytics tag

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,15 @@ class MyDocument extends Document {
                     <meta name="ahrefs-site-verification" content="8f079fd255d0ccbffee41b07433110f750f28a6b3feb8e53b188391f01c610da" />
                     <link href="https://use.typekit.net/vrn4dxs.css" rel="stylesheet" />
                     <meta name="description" content="Critical thinking, clean code and responsive design. These are the tools I use to bring digital experiences to life online." />
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0JZ5MS13QQ"></script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `window.dataLayer = window.dataLayer || [];
+                                    function gtag(){dataLayer.push(arguments);}
+                                    gtag('js', new Date());
+                                    gtag('config', 'G-0JZ5MS13QQ');`,
+                        }}
+                    />
                 </Head>
                 <body>
                     <Main />
