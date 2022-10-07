1ad30183aba01e995bbeb141d3f9ca53434e4de4
Joe di Stefano <joeydi@okaypl.us>
2022-10-07 09:39:31 -0400

    add canonical url to NextSeo

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,17 +1,20 @@
+import { useRouter } from "next/router";
 import { NextSeo } from "next-seo";
 import { getPostPaths, getPostData } from "@/lib/blog";
 import Post from "@/components/post";
 
 export default function BlogPost({ post }) {
-    const openGraph = post.frontmatter.featuredImage
-        ? {
-              images: [{ url: post.frontmatter.featuredImage?.src, alt: post.frontmatter.featuredImage?.alt }],
-          }
-        : null;
+    const router = useRouter();
+    const seo = {
+        canonical: `${process.env.BASE_URL}${router.asPath}`,
+        openGraph: {
+            images: post.frontmatter.featuredImage ? [{ url: post.frontmatter.featuredImage?.src, alt: post.frontmatter.featuredImage?.alt }] : null,
+        },
+    };
 
     return (
         <div id="content">
-            <NextSeo title={post.frontmatter.title} openGraph={openGraph} />
+            <NextSeo title={post.frontmatter.title} {...seo} />
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-md-10 col-lg-8">
